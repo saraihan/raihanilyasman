397477a5dd2171fb13d9ee5ea7c76bc2dd9dbd2c
saraihan <mr.mraihanilyasman@gmail.com>
2025-02-07 18:22:29 +0700

    Delete array directory

--- array/for_array.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nama = array("raihan","razik","rizki","ari","ria");
-$n = count($nama);
-for ($indeks=0; $indeks < $n ; $indeks++) { 
-    echo "$nama[$indeks]<br>";
-}
-?>
--- array/foreach_array.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nama = array("raihan","razik","rizki","ari","ria");
-
-foreach ($nama as $tampilnama) {
-    echo "$tampilnama <br>";
-}
-?>
