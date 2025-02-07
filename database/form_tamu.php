471ab9a3cb734f2514158bfdb77c35c5621ff874
saraihan <mr.mraihanilyasman@gmail.com>
2025-02-07 18:23:29 +0700

    Delete database directory

--- database/form_tamu.php
+++
@@ -1,7 +0,0 @@
-<h3>Form Buku Tamu</h3>
-<form method="get" action="input_tamu.php">
-    Nama: <input type="text" name="nama"><br><br>
-    E-mail: <input type="text" name="email"><br><br>
-    pesan: <textarea name="pesan" rows="5" cols="30"></textarea><br><br>
-<input type="submit" value="kirim">
-</form>
