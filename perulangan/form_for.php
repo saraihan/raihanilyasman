1057f72522bce456816726bf6913f8342b9fb5d5
saraihan <mr.mraihanilyasman@gmail.com>
2025-02-07 18:24:28 +0700

    Delete perulangan directory

--- perulangan/form_for.php
+++
@@ -1,7 +0,0 @@
-<form method="GET" action="hasil_for.php">
-
-    Berapa file yang akan di upload: <br><br>
-    <input type="text" name="jumlah_file">
-
-    <input type="submit" value="Proses">
-</form>
--- perulangan/while.php
+++
@@ -1,7 +0,0 @@
-<?php
-$i=1;
-while ($i < 10) {
-    echo "Data ke-$i <br>";
-    $i+=2;
-}
-?>
