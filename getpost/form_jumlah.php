555eef20519eda400425d4a9efbdc2c931cf2368
saraihan <mr.mraihanilyasman@gmail.com>
2025-02-07 18:24:07 +0700

    Delete getpost directory

--- getpost/form_jumlah.php
+++
@@ -1,5 +0,0 @@
-<form method="get" action="hasil_jumlah.php">
-    Nilai a: <input type="text" name="a"><br><br>
-    Nilai b: <input type="text" name="b"><br><br>
-    <input type="submit" value="Jumlahkan">
-</form>
