53edd734a57934c4ba1accc7bbb7b812580a267f
saraihan <mr.mraihanilyasman@gmail.com>
2025-02-07 18:27:09 +0700

    Delete variabel.php

--- variabel.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nama = "Muhammad Raihan Ilyasman";
-$umur = 18;
-$nilai = 92.7;
-
-echo "nama saya adalah: $nama <br>";
-echo "umur saya: $umur <br>";
-echo "nilai matematika saya: $nilai";
-?>
